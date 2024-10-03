aad8738a089e2f5f2ad44570a944d0a64815c411
Victor Oliveira <168303922+Polaroid339@users.noreply.github.com>
2024-10-03 00:55:08 -0300

    Update

--- jgdavelha.py
+++ jgdavelha.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 malha = [
     ["     ", "     ", "     "],
@@ -9,6 +10,33 @@ malha = [
 player1 = "  X  "
 player2 = "  O  "
 
+
+def checkwin(var):
+    if malha[0][0] == malha[1][1] == malha[2][2] == var:
+        print(f"O jogador {var.strip()} venceu")
+        sys.exit()
+
+    if malha[0][2] == malha[1][1] == malha[2][0] == var:
+        print(f"O jogador {var.strip()} venceu")
+        sys.exit()
+
+    for x in range(3):
+        if malha[x][0] == malha[x][1] == malha[x][2] == var:
+            print(f"O jogador {var.strip()} venceu")
+            sys.exit()
+
+    for y in range(3):
+        if malha[0][y] == malha[1][y] == malha[2][y] == var:
+            print(f"O jogador {var.strip()} venceu")
+            sys.exit()
+    for z in malha:
+        if "     " in z:
+            return False
+
+    print("O jogo empatou")
+    sys.exit()
+
+
 while True:
     print("    1       2       3")
     print(f"1 {malha[0][0]} | {malha[0][1]} | {malha[0][2]} ")
@@ -16,23 +44,31 @@ while True:
     print(f"2 {malha[1][0]} | {malha[1][1]} | {malha[1][2]} ")
     print("-" * 25)
     print(f"3 {malha[2][0]} | {malha[2][1]} | {malha[2][2]} \n")
-    print("Jogador 1\n")
 
+    print("Jogador X\n")
     while True:
         linha = int(input("Digite a linha: "))
         coluna = int(input("Digite a coluna: "))
         try:
-            if linha-1 >= 4 or coluna-1 >= 4:
-                print("Localização inválida")
+            if (linha) > 3 or (coluna) > 3:
+                print("\nLocalização inválida\n")
+
+            elif (linha) == 0 or (coluna) == 0:
+
+                print("\nLocalização inválida\n")
+
+            elif malha[linha-1][coluna-1] == "     ":
+                malha[linha-1][coluna-1] = player1
+                os.system('cls')
+                break
+
             else:
-                if malha[linha-1][coluna-1] == "     ":
-                    malha[linha-1][coluna-1] = player1
-                    os.system('cls')
-                    break
-                else:
-                    print("Localização já prenchida\n")
+                print("\nLocalização já preenchida\n")
+
         except ValueError:
             print("Caracter inválido")
+
+    checkwin(player1)
 
     print("    1       2       3")
     print(f"1 {malha[0][0]} | {malha[0][1]} | {malha[0][2]} ")
@@ -40,20 +76,28 @@ while True:
     print(f"2 {malha[1][0]} | {malha[1][1]} | {malha[1][2]} ")
     print("-" * 25)
     print(f"3 {malha[2][0]} | {malha[2][1]} | {malha[2][2]} \n")
-    print("Jogador 2\n")
 
+    print("Jogador O\n")
     while True:
         linha2 = int(input("Digite a linha: "))
         coluna2 = int(input("Digite a coluna: "))
         try:
-            if linha-1 >= 4 or coluna-1 >= 4:
-                print("Localização inválida")
+            if (linha2) > 3 or (coluna2) > 3:
+                print("\nLocalização inválida\n")
+
+            elif (linha2) == 0 or (coluna2) == 0:
+
+                print("\nLocalização inválida\n")
+
+            elif malha[linha2-1][coluna2-1] == "     ":
+                malha[linha2-1][coluna2-1] = player2
+                os.system('cls')
+                break
+
             else:
-                if malha[linha2 - 1][coluna2 - 1] == "     ":
-                    malha[linha2 - 1][coluna2 - 1] = player2
-                    os.system('cls')
-                    break
-                else:
-                    print("Localização já prenchida\n")
+                print("\nLocalização já preenchida\n")
+
         except ValueError:
             print("Caracter inválido")
+
+    checkwin(player2)
